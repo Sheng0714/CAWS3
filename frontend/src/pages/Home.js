8b8b9bdce672c0f9c2a65a57139e3616dc190f5e
Sheng0714 <168625071+Sheng0714@users.noreply.github.com>
2025-05-14 19:07:43 +0800

    更新描述（例如：修正 Notion API 錯誤）

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -332,7 +332,7 @@ export default function Home() {
             <br />
             Write Infinite Possibilities
             <br />
-            with AI
+            with A
           </h1>
         </div>
 
